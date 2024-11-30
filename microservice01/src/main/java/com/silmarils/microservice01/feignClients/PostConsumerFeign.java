a052374fc791e473ead8fee2a68dee8b8b6746f2
guiqsassi <guiqsassi@gmail.com>
2024-11-30 10:57:18 -0300

    feature: tratamento de exceptions ms-01

--- microservice01/src/main/java/com/silmarils/microservice01/feignClients/PostConsumerFeign.java
+++ microservice01/src/main/java/com/silmarils/microservice01/feignClients/PostConsumerFeign.java
@@ -1,5 +1,6 @@
 package com.silmarils.microservice01.feignClients;
 
+import com.silmarils.microservice01.config.MyFeignClientConfiguration;
 import com.silmarils.microservice01.dtos.PostCreateDto;
 import com.silmarils.microservice01.dtos.PostResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "post-consumer", url = "http://localhost:8081/api/posts")
+@FeignClient(value = "post-consumer", url = "http://localhost:8081/api/posts", configuration = MyFeignClientConfiguration.class)
 public interface PostConsumerFeign {
     @GetMapping("")
     public ResponseEntity<List<PostResponseDto>> list();
--- microservice01/src/main/java/com/silmarils/microservice01/web/controllers/PostController.java
+++ microservice01/src/main/java/com/silmarils/microservice01/web/controllers/PostController.java
@@ -5,6 +5,7 @@ import com.silmarils.microservice01.dtos.PostResponseDto;
 import com.silmarils.microservice01.feignClients.PostConsumerFeign;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,10 +32,12 @@ public class PostController {
     public ResponseEntity<?> delete(@PathVariable String id){
         return postConsumerFeign.delete(id);
     }
-    @PostMapping
-    public ResponseEntity<PostResponseDto> save(@RequestBody @Valid PostCreateDto post){
-        return postConsumerFeign.save(post);
+
+    @PostMapping("")
+    public ResponseEntity<PostResponseDto> createPost( @RequestBody PostCreateDto postCreateDto) {
+        return postConsumerFeign.save(postCreateDto);
     }
+
     @PutMapping("/{id}")
     public ResponseEntity<PostResponseDto> update(@PathVariable String id, @RequestBody @Valid PostCreateDto post){
         return postConsumerFeign.update(id, post);
