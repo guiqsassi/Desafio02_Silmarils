f34d066d498fa7e1ed7a8cddd4602c3b5d885ad4
guiqsassi <guiqsassi@gmail.com>
2024-12-01 19:32:19 -0300

    feature/ms-02 agora o endpoint sync-data tambem sincroniza comentarios

--- microservice02/src/main/java/com/silmarils/microservice02/web/dto/mapper/CommentMapper.java
+++ microservice02/src/main/java/com/silmarils/microservice02/web/dto/mapper/CommentMapper.java
@@ -4,6 +4,7 @@ package com.silmarils.microservice02.web.dto.mapper;
 import com.silmarils.microservice02.entities.Comment;
 import com.silmarils.microservice02.entities.Post;
 import com.silmarils.microservice02.web.dto.CommentCreateDto;
+import com.silmarils.microservice02.web.dto.CommentFeignDto;
 import com.silmarils.microservice02.web.dto.CommentResponseDto;
 import com.silmarils.microservice02.web.dto.CommentUpdateDto;
 import org.modelmapper.ModelMapper;
@@ -26,12 +27,30 @@ public class CommentMapper {
         return new ModelMapper().map(comment, CommentResponseDto.class);
     }
 
-    public static List<CommentResponseDto> toDto(List<Comment> comment){
+    public static List<CommentResponseDto> toListDto(List<Comment> comment){
 
         return comment.stream().map(CommentMapper::toDto).toList();
     }
 
+    public static List<Comment> toListComment(List<CommentFeignDto> comment){
+
+        return comment.stream().map(CommentMapper::toComment).toList();
+    }
+
     public static Comment toComment(CommentUpdateDto updateDto){
         return new ModelMapper().map(updateDto, Comment.class);
+    }
+
+    public static Comment toComment(CommentFeignDto dto){
+        Comment comment = new Comment();
+        comment.setId(dto.getId());
+        comment.setName(dto.getName());
+        comment.setBody(dto.getBody());
+        comment.setEmail(dto.getEmail());
+
+        Post post = new Post();
+        post.setId(dto.getPostId());
+        comment.setPostId(post);
+        return comment;
     }
 }
